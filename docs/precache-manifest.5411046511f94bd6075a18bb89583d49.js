b7df11ef92758a431255478240be28fa84d7894b
Joshua Rodriguez <ewized@gmail.com>
2019-11-09 11:10:45 -0800

    Deploy the sample react app

---
+++ docs/precache-manifest.5411046511f94bd6075a18bb89583d49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2751ab7ce4331d6d2de336e97680e001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b477b7def8877b6bc0e2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6ddb85fefe25a6f3c95e",
+    "url": "/static/js/2.63dd73f1.chunk.js"
+  },
+  {
+    "revision": "b477b7def8877b6bc0e2",
+    "url": "/static/js/main.35b89c29.chunk.js"
+  },
+  {
+    "revision": "3960cbfbc73bf2c9f8b8",
+    "url": "/static/js/runtime-main.cfc75be7.js"
+  }
+]);
